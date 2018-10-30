7575c832fc90246e306c16b69b9678f728207160
Shinveam <38589593+Shinveam@users.noreply.github.com>
2018-10-30 20:44:07 +0800

    Update example.go

--- MyExample/example.go
+++ MyExample/example.go
@@ -5,7 +5,7 @@ import (
 	"turingAPI/turing"
 )
 
-const ApiKey string = "eb58b64b8cd34a68b3c8fe588ded8191"
+const ApiKey string = "Your APIKEY"
 
 func TuringRobot() {
 	_, err := turing.Robots(ApiKey,
