cbe25e6e594ec00777dfa81fdeba04bec2f0cd25
xinwei <xinwei1210@gmail.com>
2018-10-31 19:11:30 +0800

    test

--- MyExample/example.go
+++ MyExample/example.go
@@ -6,12 +6,12 @@ import (
 	"turingAPI/turing"
 )
 
-const ApiKey string = "your apikey"
+const ApiKey string = "eb58b64b8cd34a68b3c8fe588ded8191"
 
 func TuringRobot() {
 	resp, err := turing.Robots(ApiKey,
 		turing.ReqType(0),
-		"你好啊",
+		"你好",
 	)
 	if err != nil {
 		log.Fatal(err)
